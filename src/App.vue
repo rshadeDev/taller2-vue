ef79b98fc8997f986027b17193c443bdaedba443
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-04-24 14:55:37 -0400

    desarrollo app.js

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,54 @@
 <template>
   <div>
-    <!-- Formulario de entrada para agregar nuevos nombres -->
-    <form @submit.prevent="addName">
-      <input v-model="newName" placeholder="Nombre" required>
-      <input v-model="newLastName" placeholder="Apellido" required>
+    <form @submit.prevent="agregarNombre">
+      <input v-model="nuevoNombre" placeholder="Ingresa un Nombre" required>
+      <input v-model="nuevoApellido" placeholder="Ingrea un Apellido" required>
       <button type="submit">Agregar</button>
     </form>
-    
-    <!-- Lista de nombres existentes -->
+
     <ul>
       <li v-for="(name, index) in names" :key="index">
-        {{ name.firstName }} {{ name.lastName }}
-        <button @click="editName(index)">Editar</button>
-        <button @click="deleteName(index)">Eliminar</button>
+        {{ name.nombre }} {{ name.apellido }}
+        <button @click="editarNombre(index)">Editar</button>
+        <button @click="borrarNombre(index)">Eliminar</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
+
 import { ref } from 'vue';
 
-const newName = ref('');
-const newLastName = ref('');
+const nuevoNombre = ref('');
+const nuevoApellido = ref('');
 const names = ref([]);
 
-function addName() {
-  if (newName.value && newLastName.value) {
-    names.value.push({ firstName: capitalize(newName.value), lastName: capitalize(newLastName.value) });
-    newName.value = '';
-    newLastName.value = '';
+function agregarNombre() {
+  if (nuevoNombre.value && nuevoApellido.value) {
+    names.value.push({ nombre: capitalLetter(nuevoNombre.value), apellido: capitalLetter(nuevoApellido.value) });
+    nuevoNombre.value = '';
+    nuevoApellido.value = '';
   }
 }
 
-function editName(index) {
-  const editedName = prompt('Ingrese el nuevo nombre:');
-  const editedLastName = prompt('Ingrese el nuevo apellido:');
-  if (editedName && editedLastName) {
-    names.value[index].firstName = capitalize(editedName);
-    names.value[index].lastName = capitalize(editedLastName);
+function editarNombre(index) {
+  const editarNombre = prompt("Ingrese el nuevo nombre:");
+  const editarApellido = prompt("Ingrese el nuevo apellido:");
+  
+  if (editarNombre && editarApellido) {
+    names.value[index].nombre = capitalLetter(editarNombre);
+    names.value[index].apellido = capitalLetter(editarApellido);
   }
 }
 
-function deleteName(index) {
-  names.value.splice(index, 1);
+function borrarNombre(index) {
+  if (index >= 0 && index < names.value.length) {
+    names.value.splice(index, 1);
+  }
 }
 
-function capitalize(string) {
+function capitalLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 </script>
